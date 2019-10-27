ac5ec534928c132f264a7bcade32f2c162bf6e22
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-10-27 17:19:29 +0100

    Add beer service

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -1,7 +1,8 @@
 package guru.springframework.msscbrewery.web.controller;
 
+import guru.springframework.msscbrewery.service.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +14,16 @@ import java.util.UUID;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 public class BeerController {
+
+    private final BeerService beerService;
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
 
-        return new ResponseEntity<>(BeerDto.builder().build(), OK);
+        return new ResponseEntity<>(beerService.getBeer(beerId), OK);
     }
 
 }
