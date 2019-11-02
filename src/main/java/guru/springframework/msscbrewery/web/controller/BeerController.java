bb522395208511fbe047b158b605f2db21188ae9
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-11-02 11:54:19 +0100

    Add MVC Exception Handler

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,15 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-import static java.lang.String.format;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
@@ -65,17 +59,6 @@ public class BeerController {
     @ResponseStatus(NO_CONTENT)
     public void delete(@NotNull @PathVariable("beerId") UUID beerId) {
         beerService.delete(beerId);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException exception) {
-        List<String> errors = new ArrayList<>(exception.getConstraintViolations().size());
-
-        exception.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(format("%s : %s", constraintViolation.getPropertyPath(), constraintViolation.getMessage()));
-        });
-
-        return new ResponseEntity<>(errors, BAD_REQUEST);
     }
 
 }
