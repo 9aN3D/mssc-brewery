6d001bdfd80d291ef72ce69ca8b0d3c26a07c0df
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-11-02 11:12:59 +0100

    Fix Beer Controller

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -5,7 +5,9 @@ import guru.springframework.msscbrewery.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +17,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import static java.lang.String.format;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
+@Validated
 @Deprecated
 @RestController
 @RequiredArgsConstructor
@@ -31,12 +40,12 @@ public class BeerController {
     private final BeerService beerService;
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+    public ResponseEntity<BeerDto> getBeer(@NotNull @PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeer(beerId), OK);
     }
 
     @PostMapping
-    public ResponseEntity handlePost(@Valid @RequestBody BeerDto beer) {
+    public ResponseEntity handlePost(@Valid @NotNull @RequestBody BeerDto beer) {
         BeerDto savedDto = beerService.save(beer);
 
         HttpHeaders headers = new HttpHeaders();
@@ -48,14 +57,25 @@ public class BeerController {
 
     @PutMapping("/{beerId}")
     @ResponseStatus(NO_CONTENT)
-    public void handleUpdate(@PathVariable("beerId") UUID beerId, @Valid  @RequestBody BeerDto beer) {
+    public void handleUpdate(@NotNull @PathVariable("beerId") UUID beerId, @Valid @NotNull @RequestBody BeerDto beer) {
         beerService.update(beerId, beer);
     }
 
     @DeleteMapping("/{beerId}")
     @ResponseStatus(NO_CONTENT)
-    public void delete(@PathVariable("beerId") UUID beerId) {
+    public void delete(@NotNull @PathVariable("beerId") UUID beerId) {
         beerService.delete(beerId);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException exception) {
+        List<String> errors = new ArrayList<>(exception.getConstraintViolations().size());
+
+        exception.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(format("%s : %s", constraintViolation.getPropertyPath(), constraintViolation.getMessage()));
+        });
+
+        return new ResponseEntity<>(errors, BAD_REQUEST);
     }
 
 }
